d16a13f0a6a1826f7ba6d5da6d1c08cb31696cf5
vovBorya <vovbor02@gmail.com>
2020-05-30 12:04:41 +0300

    congif prod and dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,70 +1,90 @@
-module.exports = {
-  mode: "development",
-  module: {
-    rules: [
-      /*
-      * Loader js
-       */
-      {
-        test: /\.js$/,
-        exclude: /node_moduls/,
-        use:[
-          {
-            loader: 'babel-loader'
-          }
-        ]
-      },
-      /*
-      * Loading images
-       */
-      {
-        test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: 'images',
-              name: '[name] - [sha1:hash:7].[ext]'
-            }
-          }
-          ]
-      },
-      /*
-      * Loader fonts
-       */
-      {
-        test: /\.(ttf|otf|eot|woff2)$/,
-        use: [
-          {
-            loader: "file-loader",
-            options: {
-              outputPath: 'fonts',
-              name: '[name].[ext]'
-            }
-          }
-        ]
-      },
-      /*
-      * Loader css
-       */
-      {
-        test: /\.(css)$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" }
-        ]
-      },
-      /*
-      *Loader scss
-      */
-      {
-        test: /\.(s[ca]ss)$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
-      }
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = (env = {}) => {
+
+  const { mode = 'development' } = env
+
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const getStyleLoaders = () => {
+    return [
+      isProd ? MiniCssExtractPlugin.loader: 'style-loader',
+      'css-loader'
     ]
+  }
+
+  const getPlugins = () => {
+    const plugins = [
+      new HtmlWebpackPlugin({
+        title: "Hello",
+        buildTime: new Date().toISOString(),
+        template: "public/index.html"
+      })
+    ];
+    if (isProd) {
+      plugins.push(new MiniCssExtractPlugin({
+        filename: 'main-[hash:8].css'
+      }))
+    }
+
+    return plugins;
+  }
+
+  return {
+    mode: isProd ? 'production': isDev && 'development',
+    output: {
+      filename: isProd ? "main-[hash:8].js": undefined;
+    },
+    module :
+    {
+      rules: [
+        /*
+        * Loader js
+         */
+        {
+          test: /\.js$/,
+          exclude: /node_moduls/,
+          loader: 'babel-loader'
+        },
+        /*
+        * Loading images
+         */
+        {
+          test: /\.(png|jpg|jpeg|gif|ico|svg)$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                outputPath: 'images',
+                name: '[name] - [sha1:hash:7].[ext]'
+              }
+            }
+          ]
+        },
+        /*
+        * Loader css
+         */
+        {
+          test: /\.(css)$/,
+          use: getStyleLoaders()
+        },
+        /*
+        *Loader scss
+        */
+        {
+          test: /\.(s[ca]ss)$/,
+          use: [ ...getStyleLoaders(), "sass-loader"]
+        }
+      ]
+    }
+  ,
+    plugins: getPlugins(),
+    devServer:
+    {
+      open: true,
+      port: 9000
+    }
   }
 }
